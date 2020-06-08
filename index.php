3c8e826d150a4376bfa0edafd708715616a72fef
ms-online <27732357@qq.com>
2020-06-08 20:43:34 +0800

    lesson-10

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php 
+    // 布尔值（true & false）
+    // echo true;('1')
+    // echo false;("")
 
+
+    // 比较运算符
+    // numbrs
+    // echo 1 < 5;
+    // echo 1 > 5;
+    // echo 1 == 5;
+    // echo 1 != 5;
+    // echo 1 <= 5;
+    // echo 1 >= 5;
+
+    // string
+    // echo 'summer' > 'lucy';
+    // echo 'summer' < 'lucy';
+    // echo 'Summer' < 'summer';
+    // echo 'summer' == 'summer';
+    // echo 'summer' == 'Summer';
+
+    // 松散比较:==（只比较值，不比较类型） & 严格比较:===（值和类型都要比较）
+    // echo 1 == '1';
+    // echo 1 === '1';
+    // echo 5 === 5;
+
+    // echo true == '1';
+    // echo false == '';
+    // echo true === '1';
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -10,12 +40,6 @@
     <title>PHP 初学者课程</title>
 </head>
 <body>
-        <h1>Products</h1>
-        <ul>
-        <?php  foreach($products as $product){?>
-            <h3><?php echo $product['name'];?></h3>
-            <p>¥<?php echo $product['price'];?></p>
-       <?php }?>
-        </ul>
+
 </body>
 </html>
