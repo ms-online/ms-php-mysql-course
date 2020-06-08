7d166474550b07e9baead11fecad77dc4a7bfc83
ms-online <27732357@qq.com>
2020-06-08 16:58:30 +0800

    lesson-8

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php 
-   
+//    多维数组——包含一个或者多个数组的数组
+
+    $posts = [
+        ['title'=>'post one', 'author'=>'Summer', 'content'=>'lorem', 'likes'=>20],
+        ['title'=>'post two',  'author'=>'Lucy',  'content'=>'lorem', 'likes'=>30],
+        ['title'=>'post three','author'=> 'Henry','content'=> 'lorem','likes'=> 40]
+    ];
+
+    // echo $posts[0][0];
+    // print_r($posts);
+    // print_r($posts[2]['author']);
+    // echo count($posts);
+
+    $posts[]=['title'=> 'post four','author'=>'Jeff','content'=> 'lorem','likes'=>'50'];
+    // print_r($posts);
+
+   $poped = array_pop($posts);
+   print_r($poped);
 ?>
 
 <!DOCTYPE html>
