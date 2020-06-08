fb5fb1fd5f190aadf7b9be5e17fc5742bf779997
ms-online <27732357@qq.com>
2020-06-08 23:45:29 +0800

    lesson-15

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php 
-    // 变量作用域
-
-    // 局部变量
-    function myFunc(){
-        $price = 20;
-        echo $price;
-    }
-
-    // myFunc();
-    // echo $price;
-
-    function myFuncTwo($age){
-        echo $age;
-    }
-
-    // myFuncTwo(20);
-    // echo $age;
-
-    // 全局变量
-    $name = 'Henry';
-
-    // function sayHello(){
-    //     global $name;
-    //     $name = "Bob";
-    //     echo "Hello $name";
-    // }
-    // sayHello();
-    // echo $name;
-
-    function sayBye(&$name){
-        $name = "Lucy";
-        echo "Byebye $name";
-    }
-
-    sayBye($name);
-    echo $name;
+  
 ?>
 
 <!DOCTYPE html>
