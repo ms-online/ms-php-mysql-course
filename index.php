ac6f3e34d778c422f19091cbc95dab7853b0d992
ms-online <27732357@qq.com>
2020-06-09 00:00:04 +0800

    lessson-15

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
-  
+  //include & require
+
+    include 'test.php';
+    require ('test.php');
+
+    // 文件加载失败的时候，两者不同之处：
+    // include 产生一个警告脚本并且会继续执行代码
+    // require 导致一个致命性的错误同时停止执行
+    // include('test1.php');
+    // require('test1.php');
+
+
+    echo 'end of PHP';
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +22,8 @@
     <title>PHP 初学者课程</title>
 </head>
 <body>
-       
+       <?php include('test2.php');?>
+       <?php include('test2.php');?>
+       <?php include('test2.php');?>
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php 
+    $test = ['test1', 'test2', 'test3'];
+    echo $test[2] . '<br />';
+?>
