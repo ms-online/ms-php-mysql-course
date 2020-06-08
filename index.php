2e4fc65c9add87f1dde75d37a89a8dd4866b3366
ms-online <27732357@qq.com>
2020-06-08 21:05:29 +0800

    lessson-11

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
 <?php 
-    // 布尔值（true & false）
-    // echo true;('1')
-    // echo false;("")
-
-
-    // 比较运算符
-    // numbrs
-    // echo 1 < 5;
-    // echo 1 > 5;
-    // echo 1 == 5;
-    // echo 1 != 5;
-    // echo 1 <= 5;
-    // echo 1 >= 5;
-
-    // string
-    // echo 'summer' > 'lucy';
-    // echo 'summer' < 'lucy';
-    // echo 'Summer' < 'summer';
-    // echo 'summer' == 'summer';
-    // echo 'summer' == 'Summer';
-
-    // 松散比较:==（只比较值，不比较类型） & 严格比较:===（值和类型都要比较）
-    // echo 1 == '1';
-    // echo 1 === '1';
-    // echo 5 === 5;
-
-    // echo true == '1';
-    // echo false == '';
-    // echo true === '1';
-
     
 ?>
 
