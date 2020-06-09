f574cf12551eef5c9da957f30bf189096d8cff16
ms-online <27732357@qq.com>
2020-06-09 22:16:14 +0800

    lesson-19

--- add.php
+++ add.php
@@ -5,9 +5,33 @@
     //     echo $_GET['points'] . '<br/>'; 
     // }
     if(isset($_POST['submit'])){
-        echo htmlspecialchars($_POST['email']) . '<br/>'; 
-        echo htmlspecialchars($_POST['title']) . '<br/>'; 
-        echo htmlspecialchars($_POST['points']) . '<br/>'; 
+        // echo htmlspecialchars($_POST['email']) . '<br/>'; 
+        // echo htmlspecialchars($_POST['title']) . '<br/>'; 
+        // echo htmlspecialchars($_POST['points']) . '<br/>'; 
+
+        // 验证邮箱
+        // 是否为空
+        if(empty($_POST['email'])){
+            echo '邮箱不能为空 <br/>';
+        }else{
+            echo htmlspecialchars($_POST['email']) . '<br/>';
+        }
+
+        // 验证课程名称
+        // 是否为空
+        if(empty($_POST['title'])){
+            echo '课程名称不能为空 <br/>';
+        }else{
+            echo htmlspecialchars($_POST['title']) . '<br/>';
+        }
+
+        // 验证知识要点
+        // 是否为空
+        if(empty($_POST['points'])){
+            echo '至少提交一个知识要点 <br/>';
+        }else{
+            echo htmlspecialchars($_POST['points']) . '<br/>';
+        }
     }
 ?>
 
