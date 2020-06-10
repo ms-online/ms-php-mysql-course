2c283cf8866147735f8ffd4270599cfbd77c557d
ms-online <27732357@qq.com>
2020-06-10 22:15:19 +0800

    lesson-27

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
    }
 
   // 创建SQL查询语句
-  $sql = 'SELECT title, points, id FROM courses';
+  $sql = 'SELECT title, points, id FROM courses ORDER BY created_at';
 
   // 执行查询语句得到返回结果对象
   $result = mysqli_query($conn, $sql);
@@ -24,17 +24,31 @@
   // 关闭数据库链接
   mysqli_close($conn);
 
-  print_r($courses);
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
   <?php include('templates/header.php');?>
-
-  
+   <h4 class="center grey-text">课程列表</h4>
+   <div class="container">
+   <div class="row">
+     <?php foreach($courses as $course){?>
+      <div class="col s6 md3">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6><?php echo htmlspecialchars($course['title']);?></h6>
+            <div><?php echo htmlspecialchars($course['points']);?></div>
+          </div>
+          <div class="card-action right-align">
+            <a href="#" class="brand-text">了解更多</a>
+          </div>
+        </div>
+      </div>
+     <?php }?>
+   </div>
+   </div>
+   
   <?php include('templates/footer.php');?>
      
 
