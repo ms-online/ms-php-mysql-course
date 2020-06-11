9a22547a6fcf7a046538599897cd4a999a51df9b
ms-online <27732357@qq.com>
2020-06-11 20:29:44 +0800

    lesson-40

--- sandbox.php
+++ sandbox.php
@@ -1,8 +1,35 @@
 <?php 
     // 文件系统（二）
     $file = 'readme.txt';
-    
-   
+
+    // 打开文件进行读取
+    // $handle = fopen($file, 'r');
+
+    // 读取文件
+    // echo fread($handle,filesize($file));
+    // echo fread($handle,200);
+
+    // 读取单行内容（逐行读取文件）:在调用函数后，文件指针会移动到下一行
+    // echo fgets($handle);
+    // echo fgets($handle);
+
+    // 读取单个字符（逐字符读取文件）:在调用函数后，文件指针会移动到下一个字符
+    // echo fgetc($handle);
+    // echo fgetc($handle);
+    // echo fgetc($handle);
+
+    // 读/写
+    // $handle = fopen($file, 'r+');//文件开头
+    // $handle = fopen($file, 'a+');//文件末尾进行写入操作
+
+    // 写入文件
+    //  fwrite($handle, "\nHello World");
+
+    // 关闭流
+    // fclose($handle);
+
+    // 删除文件
+    // unlink($file);
 
 ?>
 
